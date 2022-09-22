97300aef17930b548cae6a6db5deb50d05517eae
Umar Hussain <umarhussain771@gmail.com>
2022-09-22 15:42:17 +0100

    Complete step 3

---
+++ main.js
@@ -0,0 +1,37 @@
+let myLibrary = [];
+
+function Book(Title, Author, Pages, Read){
+    this.Title = Title;
+    this.Author = Author;
+    this.Pages = Pages;
+    this.Read = Read;
+    // this.info = function(){
+    //    return `${title} ${author}, ${pages}, ${read}`
+    // }
+}
+
+function addBookToLibrary(title, author, pages, read){
+    let book = new Book(title, author, pages, read);
+    myLibrary.push(book)
+}
+
+function displayBookOnPage(){
+    const books = document.querySelector('.books')
+    myLibrary.forEach(displayBook => {
+        const bookCard = document.createElement('div')
+        bookCard.classList.add('bookCard');
+        books.appendChild(bookCard)
+        for(let key in displayBook){
+            console.log(`${key}: ${myLibrary[key]}`)
+            const para = document.createElement('p')
+            para.textContent = (`${key}: ${displayBook[key]}`)
+            bookCard.appendChild(para)
+        }
+    })
+}
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295 pages", "not read yet");
+
+addBookToLibrary("Captain Underpants", "Dav Pilkey", "100 pages", "Read");
+
+displayBookOnPage();
